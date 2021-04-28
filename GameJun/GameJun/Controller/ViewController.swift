a5831b3f6c11111c83a0e8583d2c52d8c548a97e
wnsgk8300 <wnsgk8300@gmail.com>
2021-04-28 16:11:58 +0900

    setUI

---
+++ GameJun/GameJun/Controller/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  GameJun
+//
+//  Created by JEON JUNHA on 2021/04/28.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    let subjectView = UIView()
+    let subjectLabel = UILabel()
+    let setSubjectLabel = UILabel()
+    let changeButton = UIButton()
+    let selectSubjectView = UIView()
+
+    let settingView = UIView()
+    let groupLabel = UILabel()
+    let numLabel = UILabel()
+    let startButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+extension ViewController {
+    func setUI() {
+        
+    }
+}
